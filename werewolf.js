994254f7ae709d7a3b87b507659cc86f599390b6
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-23 11:18:22 -0500

    Make into class

--- werewolf.js
+++ werewolf.js
@@ -1,71 +1,78 @@
-//write player names in array 
-const players = ['tyler', 'tay', 'Ish', 'Taylor', 'Tom', 'TechleadTom', 'Tim', 'Eric'];
-
-const numberOfWerewolves = players.length < 14 ? 2 : Math.floor(players.length / 5);
-let seer = undefined, healer = undefined, werewolves = [];
-
-
-const getWerewolves = () => {
-  return new Promise((resolve, reject) => {
-    //reset array during each invocation
-    werewolves = [];
-    let remainingWerewolves = numberOfWerewolves, playersCopy = players.slice();
-
-    while (remainingWerewolves > 0) {
-      let randomPlayerIndex = Math.floor(Math.random() * Math.floor(playersCopy.length));
-
-      werewolves.push(playersCopy[randomPlayerIndex]);
-      playersCopy.splice(randomPlayerIndex, 1)
-      remainingWerewolves -= 1;
-    }
-    resolve(playersCopy);
-    reject(new Error('something went wrong in getting werewolves'))
-  })
-}
-
-
-const getSeer = (playersCopy) => {
-  return new Promise((resolve, reject) => {
-    let randomPlayerIndex = Math.floor(Math.random() * Math.floor(playersCopy.length));
-
-    seer = playersCopy[randomPlayerIndex];
-    playersCopy.splice(randomPlayerIndex, 1)
-
-    resolve(playersCopy)
-    reject(new Error('something went wrong in getting seer'))
-  })
-}
-
-
-const getHealer = (playersCopy) => {
-  return new Promise((resolve, reject) => {
-    let randomPlayerIndex = Math.floor(Math.random() * Math.floor(playersCopy.length));
-
-    healer = playersCopy[randomPlayerIndex];
-    playersCopy.splice(randomPlayerIndex, 1)
-
-    resolve(playersCopy)
-    reject(new Error('something went wrong in getting healer'))
-  })
-}
-
-
-const assignGamePlayers = () => {
-  if (players.length < 6) {
-    console.log('You need at least seven players!')
-    return;
+//write player names in array
+class Werewolf {
+  constructor(players) {
+    this.players = players;
+    this.numberOfWerewolves = players.length < 14 ? 2 : Math.floor(players.length / 5);
+    this.seer = undefined;
+    this.healer = undefined;
+    this.werewolves = [];
   }
 
-  getWerewolves().then((results) => {
-    getSeer(results).then((results) => {
-      getHealer(results).then(() => {
-        console.log(`werewolves - ${werewolves}`)
-        console.log(`the seer is ${seer}`)
-        console.log(`the healer is ${healer}`)
+  getWerewolves = () => {
+    return new Promise((resolve, reject) => {
+      //reset array during each invocation
+      this.werewolves = [];
+      let remainingWerewolves = this.numberOfWerewolves,
+        playersCopy = this.players.slice();
+
+      while (remainingWerewolves > 0) {
+        let randomPlayerIndex = Math.floor(
+          Math.random() * Math.floor(playersCopy.length)
+        );
+
+        this.werewolves.push(playersCopy[randomPlayerIndex]);
+        playersCopy.splice(randomPlayerIndex, 1);
+        remainingWerewolves -= 1;
+      }
+      resolve(playersCopy);
+      reject(new Error('something went wrong in getting werewolves'));
+    });
+  };
+
+  getSeer = (playersCopy) => {
+    return new Promise((resolve, reject) => {
+      let randomPlayerIndex = Math.floor(
+        Math.random() * Math.floor(playersCopy.length)
+      );
+
+      this.seer = playersCopy[randomPlayerIndex];
+      playersCopy.splice(randomPlayerIndex, 1);
+
+      resolve(playersCopy);
+      reject(new Error('something went wrong in getting seer'));
+    });
+  };
+
+  getHealer = (playersCopy) => {
+    return new Promise((resolve, reject) => {
+      let randomPlayerIndex = Math.floor(Math.random() * Math.floor(playersCopy.length));
+
+      this.healer = playersCopy[randomPlayerIndex];
+      playersCopy.splice(randomPlayerIndex, 1);
+
+      resolve(playersCopy);
+      reject(new Error('something went wrong in getting healer'));
+    });
+  };
+
+  assignGamePlayers = () => {
+    if (this.players.length < 6) {
+      console.log('You need at least seven players!');
+      return;
+    }
+
+    this.getWerewolves()
+      .then((results) => {
+        this.getSeer(results).then((results) => {
+          this.getHealer(results).then(() => {
+            console.log(`werewolves - ${this.werewolves}`);
+            console.log(`the seer is ${this.seer}`);
+            console.log(`the healer is ${this.healer}`);
+          });
+        });
       })
-    })
-  }).catch(err => {
-    console.log('there was an err -->', err)
-  })
+      .catch((err) => {
+        console.log('there was an err -->', err);
+      });
+  };
 }
-assignGamePlayers()
